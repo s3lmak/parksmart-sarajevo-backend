2c82d7f53e76e6942f8f47a3f85c46877b510bbe
Selma Karacic <selmakaracic@MacBook-Pro-od-Selma.local>
2026-05-12 00:16:39 +0200

    Initial Spring Boot project setup with PostgreSQL configuration

---
+++ src/main/java/ba/edu/ssst/parksmart/ParksmartBackendApplication.java
@@ -0,0 +1,13 @@
+package ba.edu.ssst.parksmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParksmartBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParksmartBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ba/edu/ssst/parksmart/ParksmartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.edu.ssst.parksmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParksmartBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
